b65e0b3b4424cb72a8fb536ec2d39511edd5631c
Steven Dalamaras <sd@stevendalamaras.dev>
2026-05-10 15:38:20 +1000

    feat(ai_eff): stub train endpoint

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ app = FastAPI()
 
 @app.post("/predict")
 def predict(payload: dict):
-    logger.info(f"Received payload: {payload}")
+    logger.info(f"Received prediction payload: {payload}")
+    # predicted_effort = model.predict(processed_payload)
     return {"predicted_effort": 42}
+
+@app.post("/train")
+def train(payload: dict):
+    logger.info(f"Received training payload: {payload.keys()}")
+    return {"training_results": "You have hit the endpoint"}
