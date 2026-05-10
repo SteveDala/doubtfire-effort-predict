2e3204cddae4d5e83c78a7dcb47e6179c8b44050
Steven Dalamaras <sd@stevendalamaras.dev>
2026-05-10 23:51:49 +1000

    docs(ai_eff): improve logging on prediction

--- app/ml/prediction.py
+++ app/ml/prediction.py
@@ -1,6 +1,9 @@
 from app.ml.features import build_features
 from app.services.model_service import get_model
 import xgboost as xgb
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def predict_raw_task_effort(task):
@@ -9,7 +12,9 @@ def predict_raw_task_effort(task):
 
     features = build_features(task)
     dtest = xgb.DMatrix(features)
-    print(features)
+    logger.info(f"New features received: {features[0]}")
+
     prediction = model.predict(dtest)[0]
+    logger.info(f"Predicted effort for task: {prediction}")
 
     return prediction
